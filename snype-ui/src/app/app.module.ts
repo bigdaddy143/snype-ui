c8bdd4919eaf6fb80c37d05bab368d32707942ac
Zachary E Buzaid <zbuzaid@gmail.com>
2018-02-17 16:41:56 -0800

    Importing Angular Material, Setting up inputs on the homepage within a search component, creating the google service, adding a config file to store api routes and keys

--- snype-ui/src/app/app.module.ts
+++ snype-ui/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
+
+// For then ng-material library compatibility
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatOptionModule, MatSelectModule,
+          MatInputModule } from '@angular/material';
+
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    // import HttpClientModule after BrowserModule.
+    HttpClientModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
